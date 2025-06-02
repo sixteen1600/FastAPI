700855fb36d952ad362bf68edfba1e6e9c800c71
sixteen1600 <zc754615@gmail.com>
2025-06-02 16:47:08 +0800

    Update firebaseConfig.js

--- week16/app/static/js/firebaseConfig.js
+++ week16/app/static/js/firebaseConfig.js
@@ -4,12 +4,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.
 
 // Firebase 設定（可依環境變數調整）
 const firebaseConfig = {
-    apiKey: "AIzaSyBeS4QmyFlY1kGk4wjJeEKA1OqZ9hsRw2k",
-    authDomain: "project-4744833769401526445.firebaseapp.com",
-    projectId: "project-4744833769401526445",
-    storageBucket: "project-4744833769401526445.appspot.com",
-    messagingSenderId: "942642616229",
-    appId: "1:942642616229:web:1cd1a43ac9f56691d2ba4c"
+    
 };
 
 // 初始化 Firebase App
